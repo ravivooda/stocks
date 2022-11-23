345bffa23ae8525f3ea6f39891ba85073ece78fd
Ravi Vooda <r.vooda@gmail.com>
2022-11-23 15:50:53 -0800

    Using quixlab theme instead

--- website/metadatas.go
+++ website/metadatas.go
@@ -6,9 +6,17 @@ import (
 )
 
 func (s *server) renderUseCases(c *gin.Context) {
-	c.HTML(http.StatusOK, useCasesTemplate, s.metadata.TemplateCustomMetadata)
+	c.HTML(http.StatusOK, useCasesTemplate, s.commonStruct())
 }
 
 func (s *server) renderDisclaimer(c *gin.Context) {
-	c.HTML(http.StatusOK, disclaimerTemplate, s.metadata.TemplateCustomMetadata)
+	c.HTML(http.StatusOK, disclaimerTemplate, s.commonStruct())
+}
+
+func (s *server) commonStruct() struct{ TemplateCustomMetadata TemplateCustomMetadata } {
+	return struct {
+		TemplateCustomMetadata TemplateCustomMetadata
+	}{
+		TemplateCustomMetadata: s.metadata.TemplateCustomMetadata,
+	}
 }
