45c45b8a9427efdcc42d08ec7223e0c95df7da28
Ravi Vooda <r.vooda@gmail.com>
2023-01-16 23:10:26 -0800

    More abstraction

--- website/letf/static/js/waits.js
+++ website/letf/static/js/waits.js
@@ -18,22 +18,17 @@ function waitForElm(selector) {
     });
 }
 
-const elems_for_hiding_on_mobile = ["1x", "1.5x", "1.25x", "1.75x", "2x", "3x", "4x", "-0.5x", "-1x", "-1.25x", "-1.5x", "-2x", "-3x", "false", "no", "variable", "other",];
-
-function setupMobileConfig() {
-    for (let i = elems_for_hiding_on_mobile.length - 1; i >= 0; i--) {
-        let elem = elems_for_hiding_on_mobile[i];
-        waitForElm('#row-select-leverage-' + elem + '_length').then((elm) => {
-            elm.classList.add("d-none")
-            elm.classList.add("d-lg-block")
-        })
-        waitForElm('#row-select-leverage-' + elem + '_filter').then((elm) => {
-            elm.classList.add("d-none")
-            elm.classList.add("d-lg-block")
-        })
-        waitForElm('#row-select-leverage-' + elem + '_info').then((elm) => {
-            elm.classList.add("d-none")
-            elm.classList.add("d-lg-block")
-        })
-    }
+function leverageConfigForMobile(elem) {
+    waitForElm(elem + '_length').then((elm) => {
+        elm.classList.add("d-none")
+        elm.classList.add("d-lg-block")
+    })
+    waitForElm(elem + '_filter').then((elm) => {
+        elm.classList.add("d-none")
+        elm.classList.add("d-lg-block")
+    })
+    waitForElm(elem + '_info').then((elm) => {
+        elm.classList.add("d-none")
+        elm.classList.add("d-lg-block")
+    })
 }
