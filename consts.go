72f83f6da70343238d64fbe43716efb0cd3bf230
ravivooda <r.vooda@gmail.com>
2023-02-13 21:21:56 -0800

    Revert "Enable multi overlaps for all while adjusting for thresholds"
    This reverts commit 3b70162b842a90715c329336a98efb38f8c3371e.

--- consts.go
+++ consts.go
@@ -14,3 +14,16 @@ var TopHardcodedETFs = []models.LETFAccountTicker{
 	"FXI",
 	"UPRO",
 }
+
+var TopHardcodedETFsMap = map[models.LETFAccountTicker]bool{
+	"TQQQ": true,
+	"XLU":  true,
+	"SOXL": true,
+	"SPY":  true,
+	"LABU": true,
+	"VXUS": true,
+	"EEM":  true,
+	"XLF":  true,
+	"FXI":  true,
+	"UPRO": true,
+}
